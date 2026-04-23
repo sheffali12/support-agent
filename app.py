9ce0551937d23d348bdb18ef6081b8b2fbefb237
sheffali12 <sheffali.a.2004@gmail.com>
2026-04-24 02:40:10 +0530

    FIx

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.chat_history import InMemoryChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.output_parsers import PydanticOutputParser
-from langchain_classic.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from contextlib import asynccontextmanager
 import uvicorn
 
